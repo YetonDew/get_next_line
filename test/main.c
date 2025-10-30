a8095313ccdaabc17fe2627e397fae8bc22438e5
YetonDew <ariasjefferson@hotmail.com>
2025-10-30 15:57:19 +0100

    test update

--- test/main.c
+++ test/main.c
@@ -10,13 +10,18 @@ int main(void)
 	char *error;
 
 	fd = open("test.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Error opening file");
+		return (1);
+	}
 	error = get_next_line(-1);
-	if (error == NULL && errno == EBADF)
+	if (error == NULL)
 		printf("Correctly handled invalid file descriptor\n");
 	else
 		printf("Failed to handle invalid file descriptor\n");
 
-	while ((line = get_next_line(fd)) != NULL) // change fd to 1 for stdout
+	while ((line = get_next_line(1	))) // change fd to 1 for stdout
 	{
 		printf("%s", line);
 		free(line);
@@ -24,3 +29,6 @@ int main(void)
 	close(fd);
 	return (0);
 }
+// cd test
+// gcc -Wall -Wextra -Werror main.c ../get_next_line.c ../get_next_line_utils.c -o demo
+// ./demo
